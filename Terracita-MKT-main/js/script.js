42fadcdd966e6d7982f917945c2793f68ca954d2
astrid-85 <85799654+astrid-85@users.noreply.github.com>
2022-02-13 16:17:24 -0300

    ejercicio Complementario

--- Terracita-MKT-main/js/script.js
+++ Terracita-MKT-main/js/script.js
@@ -1,24 +1,25 @@
 let nombrePersona ;
 let edadPersona ;
 
-let nombrePlanA = 'Estrategia';
+let nombrePlanA = "Estrategia";
+let stockTurnoA = 4 ;
 let precioPlanA = 2000;
-let stockTurnoA = 3 ;
 let descuentoPlanA = 0.8;
 
 let nombrePlanB = 'Rediseño';
 let precioPlanB = 1000;
 let stockTurnoB = 5 ;
-let descuentoPlanA = 0.7;
+let descuentoPlanB = 0.7;
 
 let nombrePlanC = 'Planificación';
 let precioPlanC = 500;
 let stockTurnoC = 10 ;
-let descuentoPlanA = 0.6;
+let descuentoPlanC = 0.6;
 
 
 
-let planEleccion = prompt(" Ingresar el Plan para consultar")
-let stockTurnoA = stockTurnoA - planEleccion;
+let planEleccion = parseInt(prompt("Ingresar el Plan para consultar"))
+stockTurnoA = parseInt(prompt("Ingresar cantidad de turno elegido"))
+stockTurnoA = stockTurnoA - planEleccion;
 
-alert("Quedan" + stockTurnoA + "Estrategia");
+alert("Quedan" + stockTurnoA + "Estrategia")
