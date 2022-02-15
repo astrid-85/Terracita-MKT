627f76af2481aa7ad1686fd94cf79e2879463444
astrid-85 <85799654+astrid-85@users.noreply.github.com>
2022-02-15 10:30:56 -0300

    Trabajo Desafio 1

--- Terracita-MKT-main/js/script.js
+++ Terracita-MKT-main/js/script.js
@@ -1,61 +1,81 @@
-let nombrePersona ;
-let edadPersona ;
+import "../js/_variable";
 
-let nombrePlanA = "Estrategia";
-let stockTurnoA = 4 ;
-let precioPlanA = 2000;
-let descuentoPlanA = 0.8;
+let nombrePersona = prompt("Ingrese su nombre y Apellido :");
+let edadPersona = parseInt (prompt("Ingrese su edad:"));
+let emailPersona = prompt("Ingrese su correo electrónico:");
 
-let nombrePlanB = 'Rediseño';
-let precioPlanB = 1000;
-let stockTurnoB = 5 ;
-let descuentoPlanB = 0.7;
+saludar(nombrePersona, edadPersona, emailPersona)
 
-let nombrePlanC = 'Planificación';
-let precioPlanC = 500;
-let stockTurnoC = 10 ;
-let descuentoPlanC = 0.6;
-
-let planEleccion ;
-let ingresarPlan;
-let precioConsulta;
-
+alert ("Estos son nuestro planes de trabajo \n" + nombrePlanA+ "\n" + nombrePlanB + "\n" + nombrePlanC );
 ingresarPlan = prompt ("Ingresar nombre del Plan de trabajo que necesitas");
+planEleccion = parseInt (prompt("Ingresar la cantidad de turnos que necesitas"));
 
 
-
+for( let i = 0; i < planEleccion; i++);{
 if (ingresarPlan == nombrePlanA){
     planEleccion = parseInt (prompt("Ingresar la cantidad de turnos que necesita"));
-    stockTurnoA = stockTurnoA - planEleccion;
-    precioConsulta = planEleccion * precioPlanA;
-    alert ("El valor de la consulta es de: $" + precioConsulta);
+   if (planEleccion <= stockTurnoA){
+        stockTurnoA = stockTurnoA - planEleccion;
+        precioConsulta = planEleccion * precioPlanA;
+        alert ("El valor de la consulta es de: $" + precioConsulta);
+   }
+    else{
+        stockInsuficiente (stockTurnoA)
+    }
     console.log ( "Quedan" + " "+ stockTurnoA + " " +"turnos para Estrategia");
 
 }
 else if ( ingresarPlan == nombrePlanB){
     planEleccion = parseInt (prompt("Ingresar la cantidad de turnos que necesita"));
-    stockTurnoB = stockTurnoB - planEleccion;
-    precioConsulta = planEleccion * precioPlanB;
     
-    alert ("El valor de la consulta es de: $" + precioConsulta);
-    
+    if (planEleccion <= stockTurnoB){
+        stockTurnoB = stockTurnoB - planEleccion;
+        precioConsulta = planEleccion * precioPlanB;
+        alert ("El valor de la consulta es de: $" + precioConsulta);
+    }
+    else{
+        stockInsuficiente (stockTurnoB)
+        }
     console.log ( "Quedan" + " "+ stockTurnoB + " " +"turnos para Rediseño");
 }     
  else if ( ingresarPlan == nombrePlanC){
     planEleccion = parseInt (prompt("Ingresar la cantidad de turnos que necesita"));
-    stockTurnoC = stockTurnoC - planEleccion;
-    precioConsulta = planEleccion * precioPlanC;
-    
-    alert ("El valor de la consulta es de: $" + precioConsulta);
-    
+    if (planEleccion <= stockTurnoC){
+        stockTurnoC = stockTurnoC - planEleccion;
+        precioConsulta = planEleccion * precioPlanC;
+        alert ("El valor de la consulta es de: $" + precioConsulta);}
+    else{
+        stockInsuficiente (stockTurnoC)   
+     }
     console.log ( "Quedan" + " "+ stockTurnoC + " " +"turnos para Planificación");
 
- }
+ } 
+
  else {
      alert("No ingresaste el nombre correcto");
  }
 
 
+}
 
 
 
+/*switch(ingresarPlan){
+    case 'Estrategia':
+        stockTurnoA = stockTurnoA - planEleccion;
+        precioConsulta = planEleccion * precioPlanA;
+        alert ("El valor de la consulta es de: $" + precioConsulta);
+        console.log ( "Quedan" + " "+ stockTurnoA + " " +"turnos para Estrategia");
+        break;
+    case 'Rediseño':
+        console.log ("Tu plan es Rediseño");
+        break;
+    case 'Planificación':
+        console.log("Tu plan es Planificación")
+        break;
+    default:
+        console.log("No elegiste ningun plan" + ingresarPlan)
+        break
+}
+
+*/
