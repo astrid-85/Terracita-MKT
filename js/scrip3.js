6ea5584a15035af3c7e72834d87078ee2ca15cd3
astrid-85 <85799654+astrid-85@users.noreply.github.com>
2022-03-16 14:02:01 -0300

    actualizacion sin terminar segunda entrega

--- js/scrip3.js
+++ js/scrip3.js
@@ -1,21 +1,21 @@
-class Presupuesto {
-    constructor(tamanio, servicio) {
-        this.tamanio = tamanio;
-        this.servicio = servicio;
+
+class Presupuesto { 
+    constructor(plan, servicio) {
+        this.plan = plan;
+        this.servicio = servicio; 
     }
 
-    //Función que calcula el presupuesto total
     presupuestar() {
         let precio;
-        switch (this.tamanio) {
+        switch (this.plan) {
             case 0:
-                precio = 500;
+                precio = 2000;
                 break;
             case 1:
-                precio = 700;
+                precio = 1000;
                 break;
             case 2:
-                precio = 800;
+                precio = 500;
                 break;
         }
         switch (this.servicio) {
@@ -34,72 +34,62 @@ class Presupuesto {
         }
         return Math.floor(precio);
     }
+
 }
+    class Interfaz{
+        static capturarDatos(){
+            const plan = document.getElementById("plan");
+            const valorPlan = parseInt(plan.options[plan.selectedIndex].value);
+            const servicio = document.getElementById("servicio");
+            const valorServicio = parseInt(servicio.options[servicio.selectedIndex].value);
 
-class Interfaz {
-    //Función que captura los datos y los devuelve limpios para ser utilizados
-    static capturarDatos() {
-        const tamanio = document.getElementById("tamanio");
-        const valorTamanio = parseInt(tamanio.options[tamanio.selectedIndex].value);
-        const servicio = document.getElementById("servicio");
-        const valorServicio = parseInt(servicio.options[servicio.selectedIndex].value);
-        //Hago destructuring, más info en https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-        return [valorTamanio, valorServicio];
-    }
+            return [valorPlan, valorServicio];
 
-    static validarDatos(valorTamanio, valorServicio) {
-        return isNaN(valorTamanio) || isNaN(valorServicio);
-    }
-
-    static colocarLoading() {
-        //Capturo el loading
-        const loading = document.getElementById("loading");
-        //Lo dejo de ocultar quitándole la clase hide
-        loading.classList.remove("hide");
-        //Al segundo lo vuelvo a ocultar poniéndole la clase hide
-        setTimeout(() => loading.classList.add("hide"), 1000);
-    }
-
-    static mostrarMensaje(mensaje, tipo = 'ok') {
-        //Creo un div con la clase mensaje para poder capturarlo luego más fácilmente cuando quiera borrarlo 
-        const div = document.createElement("div");
-        //Creo un p para colocar el resultado 
-        const p = document.createElement("p");
-        div.classList.add("mensaje");
-        if (tipo === "error") {
-            div.classList.add("mensaje--error");
-            p.textContent = mensaje;
-        } else {
-            div.classList.add("mensaje--resumen");
-            p.textContent = "total: " + mensaje;
         }
-        div.appendChild(p);
-        document.getElementById("formulario").appendChild(div);
+
+        static validarDatos (valorPlan, valorServicio){
+            return isNaN(valorPlan) || isNaN(valorServicio);
+        }
+
+        static colocarLoading(){
+            const loading = document.getElementById("loading");
+            loading.classList.remove("hide");
+            setTimeout(()=> loading.classList.add("hide"), 1000);
+        }
+
+        static mostrarMensaje(mensaje, tipo = 'ok') {
+            const div = document.createElement("div");
+            const p = document.createElement("p");
+            div.classList.add("mensaje");
+            if (tipo ==="error"){
+                div.classList.add("mensaje--error");
+                p.textContent = mensaje;
+            } else {
+                div.classList.add("mensaje--resumen");
+                p.textContent = "Tu consulta tiene el valor de:" + mensaje;
+            }
+            div.appendChild(p);
+            document.getElementById("formulario").appendChild(div);
+        }
     }
-}
+    
+    const pedirPresupuesto = (e) =>{
+        e.preventeDefault();
+        e.stopPropagation();
 
-const pedirPresupuesto = (e) => {
-    //Prevengo que se envíe la información y se recargue la pantalla
-    e.preventDefault();
-    e.stopPropagation();
-    //Almaceno los datos limpios en dos variables. Ver destructuring
-    const [valorTamanio, valorServicio] = Interfaz.capturarDatos();
+        const [valorPlan, valorServicio] = Interfaz.capturarDatos();
 
-    if (Interfaz.validarDatos(valorTamanio, valorServicio)) {
-        //Muestro un mensaje de tipo error por 2 segundos
-        Interfaz.mostrarMensaje("Por favor, complete los campos", "error");
-        setTimeout(() => document.getElementsByClassName("mensaje")[0].remove(), 2000);
-    } else {
-        //Creo un nuevo presupuesto (que podría almacenar en localStorage, por ejemplo)
-        const presupuesto = new Presupuesto(valorTamanio, valorServicio);
-        //Si hay un mensaje previo lo capturo y lo borro
-        const mensajeViejo = document.querySelector(".mensaje");
-        if (mensajeViejo) mensajeViejo.remove();
-        //Coloco un loading para que no aparezca el resultado tan repentinamente
-        Interfaz.colocarLoading();
-        //Al mismo tiempo que se va el loading hago aparecer el mensaje con el presupuesto
-        setTimeout(() => Interfaz.mostrarMensaje(presupuesto.presupuestar()), 1000);
-    }
-};
+        if (Interfaz.validarDatos(valorPlan, valorServicio)){
+            Interfaz.mostrarMensaje("Por favor, complete los campos", "error");
+            setTimeout(() => document.getElementsByClassName("mensaje")[0].remove(), 2000);
+        }else{
+            const presupuesto = new Presupuesto(valorPlan, valorServicio);
 
-document.getElementById("formulario").addEventListener("submit", pedirPresupuesto);
+            const mensajeViejo = document.querySelector(".mensaje");
+            if (mensajeViejo) mensajeViejo.remove();
+            Interfaz.colocarLoading();
+            setTimeout(()=> Interfaz.mostrarMensaje(presupuesto.presupuestar()), 1000);
+        } 
+    };
+
+    document.getElementById("formulario").addEventListener("submit", pedirPresupuesto);
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*
+
 let planEleccion;
 let precioConsulta = 0;
 let stock;
@@ -8,11 +8,13 @@ let botonPlanes = document.querySelector('.botonPlanes');
 
 
 
-function Plan (_plan, stock ,precio , descuento){
-    this.plan = _plan;
-    this.stock = stock; 
-    this.precio = precio;
-    this.descuento = descuento;
+class Plan {
+    constructor(_plan, stock, precio, descuento) {
+        this.plan = _plan;
+        this.stock = stock;
+        this.precio = precio;
+        this.descuento = descuento;
+    }
 }
 
 const planA = new Plan ("Estrategia", 4 , 2000 , 0.8)
@@ -119,109 +121,4 @@ let contacto = document.getElementsByClassName("contactosEstilo");
 
  console.log (piePagina.innerText)
 
- */
 
- 
-let planEleccion;
-let precioConsulta = 0;
-let stock;
-let precio;
-let ingresarPlan;
-let botonPlanes = document.querySelector('.botonPlanes');
-
-
-
-class Plan { 
-    constructor(plan, servicio) {
-        this.plan = plan;
-        this.servicio = servicio; 
-    }
-
-    presupuestar() {
-        let precio;
-        switch (this.plan) {
-            case 0:
-                precio = 2000;
-                break;
-            case 1:
-                precio = 1000;
-                break;
-            case 2:
-                precio = 500;
-                break;
-        }
-        switch (this.servicio) {
-            case 0:
-                precio *= 1.5;
-                break;
-            case 1:
-                precio *= 1.4;
-                break;
-            case 2:
-                precio *= 1.1;
-                break;
-            case 3:
-                precio *= 1.3;
-                break;
-        }
-        return Math.floor(precio);
-    }
-
-}
-    class Interfaz{
-        static capturarDatos(){
-            const plan = document.getElementById("plan");
-            const valorPlan = parseInt(plan.options[plan.selectedIndex].value);
-            const servicio = document.getElementById("servicio");
-            const valorServicio = parseInt(servicio.options[servicio.selectedIndex].value);
-
-            return [valorPlan, valorServicio];
-
-        }
-
-        static validarDatos (valorPlan, valorServicio){
-            return isNaN(valorPlan) || isNaN(valorServicio);
-        }
-
-        static colocarLoading(){
-            const loading = document.getElementById("loading");
-            loading.classList.remove("hide");
-            setTimeout(()=> loading.classList.add("hide"), 1000);
-        }
-
-        static mostrarMensaje(mensaje, tipo = 'ok') {
-            const div = document.createElement("div");
-            const p = document.createElement("p");
-            div.classList.add("mensaje");
-            if (tipo ==="error"){
-                div.classList.add("mensaje--error");
-                p.textContent = mensaje;
-            } else {
-                div.classList.add("mensaje--resumen");
-                p.textContent = "Tu consulta tiene el valor de:" + mensaje;
-            }
-            div.appendChild(p);
-            document.getElementById("formulario").appendChild(div);
-        }
-    }
-    
-    const pedirPresupuesto = (e) =>{
-        e.preventeDefault();
-        e.stopPropagation();
-
-        const [valorPlan, valorServicio] = Interfaz.capturarDatos();
-
-        if (Interfaz.validarDatos(valorPlan, valorServicio)){
-            Interfaz.mostrarMensaje("Por favor, complete los campos", "error");
-            setTimeout(() => document.getElementsByClassName("mensaje")[0].remove(), 2000);
-        }else{
-            const presupuesto = new presupuesto(valorPlan, valorServicio);
-
-            const mensajeViejo = document.querySelector(".mensaje");
-            if (mensajeViejo) mensajeViejo.remove();
-            Interfaz.colocarLoading();
-            setTimeout(()=> Interfaz.mostrarMensaje(presupuesto.presupuestar()), 1000);
-        } 
-    };
-
-    document.getElementById("formulario").addEventListener("submit", pedirPresupuesto);
