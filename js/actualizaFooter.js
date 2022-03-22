b600fc2631512315b09ff8fe053380df3ee2a200
astrid-85 <85799654+astrid-85@users.noreply.github.com>
2022-03-22 19:29:33 -0300

    actualizacion

--- js/actualizaFooter.js
+++ js/actualizaFooter.js
@@ -1,7 +1,4 @@
 
-/**let boton1 = document.querySelector ('.boton1')
- boton1.addEventListener('click', Bienvenido)*/
-
  let piePagina = document.querySelector("#pie3  p")
 
  piePagina.innerText = "Copyright-Terracita 2022"
