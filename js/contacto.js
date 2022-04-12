ab44ad3f4b0abb2477e7f428b9d4d05c3a459b8d
astrid-85 <85799654+astrid-85@users.noreply.github.com>
2022-04-12 18:02:46 -0300

    Trabajo Final Entrega Curso javascript

--- js/contacto.js
+++ js/contacto.js
@@ -1,65 +1,54 @@
-let usuarioPlan = []
+let usuarioPlan = [];
 
-if (localStorage.getItem('planUsuario')) {
-    usuarioPlan = JSON.parse(localStorage.getItem('planUsuario'))
+if (localStorage.getItem("planUsuario")) {
+  usuarioPlan = JSON.parse(localStorage.getItem("planUsuario"));
 } else {
-    localStorage.setItem('planUsuario', JSON.stringify(usuarioPlan))
+  localStorage.setItem("planUsuario", JSON.stringify(usuarioPlan));
 }
 
-let formulario = document.getElementById("formularioUno")
-let botonMostrar = document.getElementById("botonMostrar")
-let divSeleccion = document.getElementById("seleccionPlan")
-let enviar = document.getElementById("enviar")
+let formulario = document.getElementById("formularioUno");
+let botonMostrar = document.getElementById("botonMostrar");
+let divSeleccion = document.getElementById("seleccionPlan");
+let enviar = document.getElementById("enviar");
 
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
 
-    let fullname = document.getElementById('nombre_apellido').value
-    let email = document.getElementById('correo_electronico').value
-    let planSelected = document.getElementById('plan').value
-    let appointment = document.getElementById('cantidadTurno').value
-    
-    
-    if (!fullname) { return showError('Nombre completo') }
-    if (!email.includes('@') && email.length > 4) { return showError('Email') }
+  let fullname = document.getElementById("nombre_apellido").value;
+  let email = document.getElementById("correo_electronico").value;
+  let planSelected = document.getElementById("plan").value;
+  let appointment = document.getElementById("cantidadTurno").value;
 
-        const usuario = new User (fullname, email, planSelected, appointment)
-        usuarioPlan.push(usuario)
-        localStorage.setItem('planUsuario', JSON.stringify(usuarioPlan))
-        formulario.reset()
-        displayMessage(usuario)
-})
 
-botonMostrar.addEventListener('click', () => {
-  
-   if(usuarioPlan.length){
-        usuarioPlan.forEach((User, indice) => {
-            displayMessage(User);
-           
-        })
-   } 
-})
+  const usuario = new User(fullname, email, planSelected, appointment);
+  usuarioPlan.push(usuario);
+  localStorage.setItem("planUsuario", JSON.stringify(usuarioPlan));
+  formulario.reset();
+  displayMessage(user);
+});
 
-function displayMessage (user) {
-return divSeleccion.innerHTML = `         
+botonMostrar.addEventListener("click", () => {
+  if (usuarioPlan.length) {
+    usuarioPlan.forEach((user) => {
+      displayMessage(user);
+    });
+  }
+});
+
+function displayMessage(user) {
+  return (divSeleccion.innerHTML = `         
 
             <div class="card">
             Hola ${user.fullname}, ¿cómo estás? Elegiste el Plan: ${user.planSelected}, 
             solicitaste esta cantidad de turno ${user.appointment}.
             Responderemos en breve tu consulta a ${user.email} . 
             </div>
-            `
+            `);
 }
 
-function showError(error) {
-    return divSeleccion.innerHTML = `         
-    <div class="alert alert-danger">
-    Debe completar todos los campos. falta <b>${error}</b>
-    </div>
-    `
-}
 
-let piePagina = document.querySelector("#pie3  p")
 
-piePagina.innerText = "Copyright-Terracita 2022"
+let piePagina = document.querySelector("#pie3  p");
+
+piePagina.innerText = "Copyright-Terracita 2022";
